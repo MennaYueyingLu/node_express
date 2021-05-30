01b7717fba5bf50c64974624d965f7856cdd912e
YYLu <menna.yueying.lu@gmail.com>
2021-05-30 11:19:27 +0200

    first commit

---
+++ routes/promoRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyparser.json());
+
+promoRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end("will send all promotions to you");
+})
+.post((req,res, next)=>{
+    res.end('Will add the promotion:' + req.body.name + ' with description:' + req.body.description);
+})
+.put((req,res, next)=>{
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req,res, next)=>{
+    res.end('Deleting all promotions');
+});
+
+promoRouter.route('/:promoId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end('Will send details of the promotions: ' + req.params.promoId +' to you!')
+})
+.post((req,res, next)=>{
+    res.end('POST operation not supported on /promotions/'+ req.params.promoId);
+})
+.put((req,res, next)=>{
+    res.write('Updating the promotion: ' + req.params.promoId + '\n');
+    res.end('Will update the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req,res, next)=>{
+    res.end('Deleting promotion: ' + req.params.promoId);
+});
+
+module.exports = promoRouter;
